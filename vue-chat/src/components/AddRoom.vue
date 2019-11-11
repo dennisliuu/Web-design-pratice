98ea3a41829595de93dfba28fe7bf9b934ea7a4b
dennisliuu <dennisliuu@gmail.com>
2019-11-12 00:08:49 +0800

    finish add room

--- vue-chat/src/components/AddRoom.vue
+++ vue-chat/src/components/AddRoom.vue
@@ -1,52 +1,48 @@
 <template>
-  <b-row>
-    <b-col cols="12">
-      <h2>
-        Add Room
-        <b-link @click="$router.go(-1)">(Room List)</b-link>
-      </h2>
-      <b-jumbotron>
-        <b-form @submit="onSubmit">
-          <b-form-group>
-            <b-form-input id="roomname" v-model.trim="room.roomName" placeholder="Enter Room Name"></b-form-input>
-          </b-form-group>
-          <b-button type="submit" variant="primary" :disabled="!room.roomName">Save</b-button>
-        </b-form>
-      </b-jumbotron>
-    </b-col>
-  </b-row>
+  <div class="container my-md-3">
+    <h2>
+      Add Room
+      <b-link @click="$router.go(-1)">(Room List)</b-link>
+    </h2>
+    <b-jumbotron class="bg-primary-lighter">
+      <b-form @submit="onSubmit">
+        <b-input-group>
+          <b-form-input style="width: 50vw" id="roomname" v-model.trim="room.roomName" placeholder="Enter Room Name"></b-form-input>
+          <b-button squared type="submit" variant="outline-success" :disabled="!room.roomName">Save</b-button>
+        </b-input-group>
+      </b-form>
+    </b-jumbotron>
+  </div>
 </template>
 <script>
-
-import firebase from '../Firebase'
-import router from '../router'
+import firebase from "../Firebase";
+import router from "../router";
 
 export default {
-  name: 'AddBoard',
-  data () {
+  name: "AddBoard",
+  data() {
     return {
-      ref: firebase.database().ref('chatrooms/'),
-      room: { roomName: '' }
-    }
+      ref: firebase.database().ref("chatrooms/"),
+      room: { roomName: "" }
+    };
   },
   methods: {
-    onSubmit (evt) {
-      evt.preventDefault()
+    onSubmit(evt) {
+      evt.preventDefault();
 
-      let newData = this.ref.push()
+      let newData = this.ref.push();
       newData.set({
         roomName: this.room.roomName
-      })
-      router.go(-1)
-      .catch((error) => {
-        alert("Error adding document: ", error)
+      });
+      router.go(-1).catch(error => {
+        alert("Error adding document: ", error);
       });
     }
   }
-}
+};
 </script>
 <style>
-  .jumbotron {
-    padding: 2rem;
-  }
+.jumbotron {
+  padding: 2rem;
+}
 </style>
